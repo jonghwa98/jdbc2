c666e06b60f40b181f742456b3750bac0ddfd2f5
jonghwa98 <parkjh3694@gmail.com>
2023-06-20 15:39:09 +0900

    #6 dto

---
+++ src/koreait/jdbc/day4/JCustomer.java
@@ -0,0 +1,20 @@
+package koreait.jdbc.day4;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+public class JCustomer {		// DTO : JBuy, JProduct
+	private String custom_id;
+	private String name;
+	private String email;
+	private int age; 
+	private Date reg_date;
+}
